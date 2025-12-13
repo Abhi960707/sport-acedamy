28cc4d569d3f08d2540a9c0b2c648d2c4998666a
Abhijeet Narsale <abhijeetnarsale14082004@gmail.com>
2025-12-13 16:25:42 +0530

    Mongo Atlas Connection For Deploying

--- Backend/Database/db.js
+++ Backend/Database/db.js
@@ -1,9 +1,29 @@
-const mongoose = require('mongoose')
+// This code only work vscode not work in deployement on any platform so commit keli
+// const mongoose = require('mongoose')
 
-const URL = 'mongodb://127.0.0.1:27017/Login'
-const serverdb = mongoose.connect(URL)
+// const URL = 'mongodb://127.0.0.1:27017/Login'
+// const serverdb = mongoose.connect(URL)
 
-if(serverdb)
-    console.log('connected to DB')
-else
-    console.log('connection failed to DB')
+// if(serverdb)
+//     console.log('connected to DB')
+// else
+//     console.log('connection failed to DB')
+
+
+//2
+// const mongoose = require('mongoose');
+
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => console.log("MongoDB Connected Successfully"))
+//   .catch((err) => console.log("DB Connection Failed:", err));
+
+  
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB Connected Successfully');
+  })
+  .catch((err) => {
+    console.log('DB Connection Failed:', err.message);
+  });
